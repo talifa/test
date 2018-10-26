13887a0cc1f7f627e3bf653ed61ddb0c7ea18998
t.ausheva <t.ausheva@astonconsulting.ru>
2018-10-26 17:50:54 +0300

    delete item

--- js/common.js
+++ js/common.js
@@ -1,29 +1,30 @@
-function closeSelect(selectMenu,listItems) {
+function closeSelect(selectMenu, listItems) {
     $(selectMenu).removeClass('active');
-    TweenMax.staggerTo(listItems,0.12,{y:-20,opacity:0,onComplete:function(){listItems.css("display","none");}},-0.05);
-}
-function openSelect(selectMenu,listItems) {
-    $(selectMenu).addClass('active');
-    listItems.css("display","block");
-    TweenMax.staggerFromTo(listItems,0.12,{opacity:0,y:-20,ease:Back.easeOut},{opacity:1,y:0},0.05);
+    TweenMax.staggerTo(listItems, 0.12, { y: -20, opacity: 0, onComplete: function() { listItems.css("display", "none"); } }, -0.05);
 }
 
-$('select').each(function(){
-    var $this = $(this), 
+function openSelect(selectMenu, listItems) {
+    $(selectMenu).addClass('active');
+    listItems.css("display", "block");
+    TweenMax.staggerFromTo(listItems, 0.12, { opacity: 0, y: -20, ease: Back.easeOut }, { opacity: 1, y: 0 }, 0.05);
+}
+
+$('select').each(function() {
+    var $this = $(this),
         numOptions = $this.children('option').length;
-  
-    $this.addClass('select-hidden'); 
+
+    $this.addClass('select-hidden');
     $this.wrap('<div class="select"></div>');
     $this.after('<div class="select-styled"></div>');
 
     var $styledSelect = $this.next('div.select-styled');
     $styledSelect.text($this.children('option').eq(0).text());
-    
+
     //Create List
     var $list = $('<ul />', {
         'class': 'select-options'
     }).insertAfter($styledSelect);
-  
+
     //Populate List
     for (var i = 1; i < numOptions; i++) {
         $('<li />', {
@@ -31,30 +32,43 @@ $('select').each(function(){
             rel: $this.children('option').eq(i).val()
         }).appendTo($list);
     }
-  
+
     var $listItems = $list.children('li');
-  
-    closeSelect($('.select-styled'),$listItems);
-  
+
+    closeSelect($('.select-styled'), $listItems);
+
     $styledSelect.click(function(e) {
         e.stopPropagation();
-        if($(this).hasClass('active')) {
-            closeSelect(this,$listItems);
+        if ($(this).hasClass('active')) {
+            closeSelect(this, $listItems);
         } else {
-            openSelect(this,$listItems);
+            openSelect(this, $listItems);
         }
     });
-  
+
     $listItems.click(function(e) {
         e.stopPropagation();
         $styledSelect.text($(this).text()).removeClass('active');
         $this.val($(this).attr('rel'));
-        closeSelect($('.select-styled'),$listItems);
+        closeSelect($('.select-styled'), $listItems);
     });
-  
+
     $(document).click(function() {
-        closeSelect($('.select-styled'),$listItems);
+        closeSelect($('.select-styled'), $listItems);
     });
 
 });
 
+$('.cell_mobile .delete_btn').click(function() {
+    $(this).parent().parent().parent().css({
+        display: 'none',
+
+    });
+});
+$('.cell .delete_btn').click(function() {
+    $(this).parent().parent().css({
+        display: 'none',
+
+
+    });
+});
